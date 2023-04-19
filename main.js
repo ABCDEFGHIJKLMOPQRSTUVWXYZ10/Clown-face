7206d9450d827e02cf774cf1262939136ccdfc12
ABCDEFGHIJKLMOPQRSTUVWXYZ10 <125325846+ABCDEFGHIJKLMOPQRSTUVWXYZ10@users.noreply.github.com>
2023-04-19 17:48:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+    function preloaded(){
+
+    }
+    function setup(){
+        canvas = createCanvas(300, 300);
+        canvas.center();
+    }
+    function draw(){
+
+    }
+    function take_snapshot(){
+        save("myFilterImage.png");
+    }
